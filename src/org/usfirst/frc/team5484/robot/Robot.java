d6453cbefcada0eafc315922979d19852fe26342
Roger Johnson <rogermct@hotmail.com>
2018-01-02 21:27:45 -0500

    Added code to turn by button press.

--- src/org/usfirst/frc/team5484/robot/Robot.java
+++ src/org/usfirst/frc/team5484/robot/Robot.java
@@ -3,8 +3,11 @@ package org.usfirst.frc.team5484.robot;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
@@ -15,10 +18,19 @@ import com.kauailabs.navx.frc.AHRS;
  * This is a demo program showing how to use Mecanum control with the RobotDrive
  * class.
  */
-public class Robot extends SampleRobot {
+public class Robot extends SampleRobot implements PIDOutput {
 	AHRS ahrs;
 	RobotDrive robotDrive;
 	CameraServer camServer;
+	PIDController turnController;
+	double rotateToAngleRate;
+	
+	static final double kP = 0.03;
+    static final double kI = 0.00;
+    static final double kD = 0.00;
+    static final double kF = 0.00;
+    
+    static final double kToleranceDegrees = 2.0f;
 
 	// Channels for the wheels
 	final int kFrontLeftChannel = 9;
@@ -46,6 +58,13 @@ public class Robot extends SampleRobot {
 		{
 			DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true);
 		}
+		turnController = new PIDController(kP, kI, kD, kF, ahrs, this);
+        turnController.setInputRange(-180.0f,  180.0f);
+        turnController.setOutputRange(-1.0, 1.0);
+        turnController.setAbsoluteTolerance(kToleranceDegrees);
+        turnController.setContinuous(true);
+        
+        LiveWindow.addActuator("DriveSystem", "RotateController", turnController);
 	}
 
 	/**
@@ -55,6 +74,7 @@ public class Robot extends SampleRobot {
 	public void operatorControl() {
 		robotDrive.setSafetyEnabled(true);
 		while (isOperatorControl() && isEnabled()) {
+			boolean rotateToAngle = false;
 			double x = stick.getX();
 			double y = stick.getY();
 			double z = stick.getZ();
@@ -73,16 +93,49 @@ public class Robot extends SampleRobot {
 			{
 				ahrs.reset();
 			}
+			if(stick.getRawButton(7))
+			{
+				turnController.setSetpoint(90.0f);
+				rotateToAngle = true;
+			}
+			else if ( stick.getRawButton(9)) 
+			{
+                turnController.setSetpoint(179.9f);
+                rotateToAngle = true;
+            } 
+			else if ( stick.getRawButton(11)) 
+			{
+                turnController.setSetpoint(-90.0f);
+                rotateToAngle = true;
+            }
+            double currentRotationRate;
+            if ( rotateToAngle ) 
+            {
+                turnController.enable();
+                currentRotationRate = rotateToAngleRate;
+            } 
+            else 
+            {
+                turnController.disable();
+                currentRotationRate = stick.getTwist();
+            }
 			try {
-				robotDrive.mecanumDrive_Cartesian(x, y, z, ahrs.getAngle());
+				robotDrive.mecanumDrive_Cartesian(x, y, currentRotationRate, ahrs.getAngle());
 			}
 			catch(RuntimeException ex) {
 				DriverStation.reportError("Error communicating with drive system:  " + ex.getMessage(), true);
 			}
 		}
-			
-			
-
-			Timer.delay(0.005); // wait 5ms to avoid hogging CPU cycles
+		Timer.delay(0.005); // wait 5ms to avoid hogging CPU cycles
 	}
+	
+	public void test() {
+	}
+	
+	@Override
+    /* This function is invoked periodically by the PID Controller, */
+    /* based upon navX MXP yaw angle input and PID Coefficients.    */
+    public void pidWrite(double output) {
+        rotateToAngleRate = output;
+    }
 }
